c8168221d3e268a28c2a5b1c6bd54a33fafa6655
Kyle Thorpe <kjthorpe18@gmail.com>
2024-05-01 22:04:56 -0400

    Add encoding and decoding logic to services
    Handle encoding URLs and storing them in the DB, or finding previously encoded URLs for a given URL.
    Handle decoding URLs, returning the original or throwing an exception if this URL has not been seen before, possibly having not been generated by this service

--- src/main/java/com/kthorpe/urlshortener/service/DecoderService.java
+++ src/main/java/com/kthorpe/urlshortener/service/DecoderService.java
@@ -1,10 +1,36 @@
 package com.kthorpe.urlshortener.service;
 
+import com.kthorpe.urlshortener.entity.UrlEntity;
+import com.kthorpe.urlshortener.exception.DecodingException;
+import com.kthorpe.urlshortener.repository.IUrlRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class DecoderService {
-    public String decodeUrl(String url) {
-        return url + "-decoded";
+
+    @Autowired IUrlRepository urlRepository;
+
+    /**
+     * Given an encoded URL, search the DB for its original version
+     *
+     * @param encodedUrl The encoded URL
+     * @return The corresponding original URL
+     * @throws DecodingException if an encoded URL has not been seen before
+     */
+    public String decodeUrl(String encodedUrl) throws DecodingException {
+        UrlEntity urlEntity = urlRepository.findByEncodedUrl(encodedUrl);
+
+        if (urlEntity == null) {
+            log.error("Encoded URL not found in repository");
+            throw new DecodingException("URL not found");
+        }
+
+        log.info("Encoded URL found in repository. Returning original URL");
+        return urlEntity.getUrl();
     }
 }
